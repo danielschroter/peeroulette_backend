fab9ef31cf908d22b33ec8d501816990c8651716
Benjamin Greif <b.greif@mailbox.org>
2021-06-13 09:02:31 +0200

    register or create user is working for new /user api route in backend

---
+++ src/routes/user.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express  = require('express');
+const router   = express.Router();
+
+const middlewares    = require('../middlewares');
+const UserController = require('../controllers/user');
+
+
+router.post("/register", UserController.register); // register a new user
+router.get('/', UserController.list); // List all movies
+router.post('/create', middlewares.checkAuthentication, UserController.create); // Create a new movie
+router.get('/read', UserController.read); // Read a movie by Id
+router.put('/update', middlewares.checkAuthentication, UserController.update); // Update a movie by Id
+router.delete('/delete', middlewares.checkAuthentication, UserController.remove); // Delete a movie by Id
+
+
+module.exports = router;
