ab8c2a6bfb07e0b282df796f342023a502721bbf
Benjamin Greif <b.greif@mailbox.org>
2021-06-13 15:02:08 +0200

    no progresson update, but register is working

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,7 +11,7 @@ router.post("/register", UserController.register); // register a new user
 router.get('/', UserController.list); // List all movies
 router.post('/create', middlewares.checkAuthentication, UserController.create); // Create a new movie
 router.get('/read', UserController.read); // Read a movie by Id
-router.put('/update', middlewares.checkAuthentication, UserController.update); // Update a movie by Id
+router.put('/update', UserController.update); // Update a movie by Id
 router.delete('/delete', middlewares.checkAuthentication, UserController.remove); // Delete a movie by Id
 
 
