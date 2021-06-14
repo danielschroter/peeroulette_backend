a4b2a78c3ff0e481961c9b346903543669a8c2f4
Benjamin Greif <b.greif@mailbox.org>
2021-06-14 18:40:54 +0200

    removed redundant code in backend

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,25 +7,16 @@ const middlewares    = require('../middlewares');
 const UserController = require('../controllers/user');
 
 
-router.post("/register", UserController.register); // register a new user
-
-router.get("/", UserController.list); // List all movies
-router.post(
-    "/",
-    middlewares.checkAuthentication,
-    middlewares.checkIsAdmin,
-    UserController.create
-); // Create a new movie, needs logged in user with the admin role
-router.get("/:id", UserController.read); // Read a movie by Id
+router.get("/", UserController.list); // List all users
+router.get("/:id", UserController.read); // Read a user by Id
 router.put(
     "/:id",
     UserController.update
-); // Update a movie by Id, needs logged in user with the admin role
+); // Update a user by Id, needs logged in user with the admin role
 router.delete(
     "/:id",
-
     UserController.remove
-); // Delete a movie by Id, needs logged in user with the admin role
+); // Delete a user by Id, needs logged in user with the admin role
 
 
 module.exports = router;
