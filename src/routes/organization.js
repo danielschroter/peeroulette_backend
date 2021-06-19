0f3c372fe0a9d1d58f6bbba314a6cf84f8a33e64
Benjamin Greif <b.greif@mailbox.org>
2021-06-19 13:00:22 +0200

    adjusted backend so that organization can be deleted and updated in the frontened, as the organization functionality was already implemented, I only added a new api route to the organizations which are accessible to change via the UserService

--- src/routes/organization.js
+++ src/routes/organization.js
@@ -11,7 +11,7 @@ router.get('/', OrganizationController.list); // List all movies
 router.post('/', middlewares.checkAuthentication, OrganizationController.create); // Create a new movie
 router.get('/:id', OrganizationController.read); // Read a movie by Id
 router.put('/:id', middlewares.checkAuthentication, OrganizationController.update); // Update a movie by Id
-router.delete('/:id', middlewares.checkAuthentication, OrganizationController.remove); // Delete a movie by Id
+router.delete('/:id', OrganizationController.remove); // Delete an organization by Id
 
 
 module.exports = router;
