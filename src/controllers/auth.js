2c8231fd1a0886136fe390f118ab2f9cc26100a6
Benjamin Greif <b.greif@mailbox.org>
2021-06-27 11:46:03 +0200

    added functionality to register organisation only in backend

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -130,7 +130,79 @@ const register = async (req, res) => {
     }
 };
 
+const registerOrganization = async (req, res) => {
+    // check if the body of the request contains all necessary properties
+    if (!Object.prototype.hasOwnProperty.call(req.body, "password"))
+        return res.status(400).json({
+            error: "Bad Request",
+            message: "The request body must contain a password property",
+        });
 
+    if (!Object.prototype.hasOwnProperty.call(req.body, "username"))
+        return res.status(400).json({
+            error: "Bad Request",
+            message: "The request body must contain a username property",
+        });
+
+    // handle the request
+    try {
+        // hash the password before storing it in the database
+        const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+        // create a user object
+        const user = {
+            username: req.body.username,
+            password: hashedPassword,
+            role: req.body.isAdmin ? "admin" : "member",
+        };
+
+        // create the user in the database
+        let retUser = await UserModel.create(user);
+
+        if (req.body.compname != ""){
+            const org = {
+                company_name: req.body.compname,
+                account_owner: retUser._id,
+                domains: [req.body.domains],
+            };
+
+            let retOrg = await OrganizationModel.create(org);
+            await UserModel.findOneAndUpdate({_id:retUser._id}, {account_owner_of_organization:retOrg._id}, {new: true})
+        }
+
+        // if user is registered without errors
+        // create a token
+        const token = jwt.sign(
+            {
+                _id: retUser._id,
+                username: retUser.username,
+                role: retUser.role,
+            },
+            config.JwtSecret,
+            {
+                expiresIn: 86400, // expires in 24 hours
+            }
+        );
+
+        // return generated token
+        res.status(200).json({
+            token: token,
+        });
+    } catch (err) {
+        console.log('Getting in here');
+        if (err.code == 11000) {
+            return res.status(400).json({
+                error: "User exists",
+                message: err.message,
+            });
+        } else {
+            return res.status(500).json({
+                error: "Internal server error",
+                message: err.message,
+            });
+        }
+    }
+};
 
 const me = async (req, res) => {
     try {
@@ -159,6 +231,7 @@ const logout = (req, res) => {
 };
 
 module.exports = {
+    registerOrganization,
     login,
     register,
     logout,
--- src/routes/organization.js
+++ src/routes/organization.js
@@ -13,5 +13,4 @@ router.get('/:id', OrganizationController.read); // Read a movie by Id
 router.put('/:id', middlewares.checkAuthentication, OrganizationController.update); // Update a movie by Id
 router.delete('/:id', OrganizationController.remove); // Delete an organization by Id
 
-
 module.exports = router;
