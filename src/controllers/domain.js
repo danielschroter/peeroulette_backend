f108fbcb690d66a0ef273ecf3a1d81aa3f50a15b
Benjamin Greif <b.greif@mailbox.org>
2021-07-07 19:23:58 +0200

    worked on fixing bug that domains are not added to the organisations in the backend, but not fixed yet

--- src/controllers/domain.js
+++ src/controllers/domain.js
@@ -34,7 +34,49 @@ const read = async (req, res) => {
     }
 };
 
+const create = async (req, res) => {
+    if (Object.keys(req.body).length === 0) return res.status(400).json({
+        error: 'Bad Request',
+        message: 'The request body is empty'
+    });
+
+    // handle the request
+    try {
+        let domain = await DomainModel.create(req.body.domain)
+        // return new domain
+        res.status(200).json(domain);
+    } catch (err) {
+        console.log("Getting in here");
+        if (err.code == 11000) {
+            return res.status(400).json({
+                error: "Domain exists",
+                message: err.message,
+            });
+        } else {
+            return res.status(500).json({
+                error: "Internal server error",
+                message: err.message,
+            });
+        }
+    }
+};
+
+const remove = async (req, res) => {
+    try {
+        await DomainModel.findByIdAndRemove(req.params.id).exec();
+
+        return res.status(200).json({message: `domain with id${req.params.id} was deleted`});
+    } catch(err) {
+        return res.status(500).json({
+            error: 'Internal server error',
+            message: err.message
+        });
+    }
+};
+
 module.exports = {
+    create,
     list,
     read,
+    remove,
 };
--- src/routes/domain.js
+++ src/routes/domain.js
@@ -7,5 +7,7 @@ const DomainController = require('../controllers/domain');
 
 router.get("/", DomainController.list); // list domains
 router.get("/:id", DomainController.read); // list domains
+router.delete('/:id', DomainController.remove); // Delete an organization by Id
+router.post("/addDomain", DomainController.create); // register a new user
 
 module.exports = router;
