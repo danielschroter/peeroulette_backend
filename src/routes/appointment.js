0597cf70c24d94f7949e560476d2fd489372b3b2
Daniel Schroter <daniel.schroter@tum.de>
2021-07-13 16:57:46 +0200

    FIX: Rounting more specific to get appointments

--- src/routes/appointment.js
+++ src/routes/appointment.js
@@ -5,6 +5,6 @@ const router   = express.Router();
 
 const AppointmentController = require('../controllers/appointment');
 
-router.get("/", AppointmentController.getAppointments); // List all users
+router.get("/getAppointments", AppointmentController.getAppointments); // List all users
 
 module.exports = router;
