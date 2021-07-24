984ebfa5c52e50a7b9bfa91f53565c948c4b9fea
Daniel Schroter <daniel.schroter@tum.de>
2021-07-24 11:41:26 +0000

    Revert "Merge branch 'UC_Iceabreaker_Game' into 'master'"
    This reverts merge request !18

--- src/config.js
+++ src/config.js
@@ -5,12 +5,8 @@ const port = process.env.PORT || "4000";
 const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/moviedb";
 const JwtSecret = process.env.JWT_SECRET || "very secret secret";
 
-// Frontend API for emailTemplate.js
-const frontendURL = "http://localhost:3000";
-
 module.exports = {
-  port,
-  mongoURI,
-  JwtSecret,
-  frontendURL,
+    port,
+    mongoURI,
+    JwtSecret,
 };
