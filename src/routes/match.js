2669751f07d59b45e4f24133d58622125e4e5615
Benedikt Stelzl <benedikt.stelzl@tum.de>
2021-07-25 17:17:49 +0200

    messernger backend working

--- src/routes/match.js
+++ src/routes/match.js
@@ -11,7 +11,7 @@ router.get('/', MatchController.list); // List all matches
 router.post('/', MatchController.create); // Create a new match
 router.get('/:id', MatchController.read); // Read a match by Id
 router.put('/:id', middlewares.checkAuthentication, MatchController.update); // Update a match by Id
-router.delete('/:id', middlewares.checkAuthentication, MatchController.remove); // Delete a match by Id
+router.delete('/:id', MatchController.remove); // Delete a match by Id
 router.get('/last/:userId', MatchController.getLastMatches); // Get last matches of user
 
 
